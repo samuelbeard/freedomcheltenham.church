02763d06c8a64c6a5a26e8ee394385b04985eb61
Samuel Beard <sam@charisma.ai>
2024-02-20 15:49:34 +0000

    fix email address

--- app/(website)/contact/page.tsx
+++ app/(website)/contact/page.tsx
@@ -7,31 +7,31 @@ export const metadata = {
 export default function Page() {
     return (
         <>
-            <section className="section bg-[url('/img/photos/smiling.jpg')] md:bg-[url('/img/photos/smiling-cropped.jpg')] bg-cover md:bg-center">
-                <div className="bg-black w-full py-24 bg-opacity-50">
-                    <h1 className="h1 text-6xl text-white text-center">
+            <section className="section bg-[url('/img/photos/smiling.jpg')] bg-cover md:bg-[url('/img/photos/smiling-cropped.jpg')] md:bg-center">
+                <div className="w-full bg-black bg-opacity-50 py-24">
+                    <h1 className="h1 text-center text-6xl text-white">
                         Get in Contact
                     </h1>
                 </div>
             </section>
             <div className="section py-20">
-                <div className="xl:container mx-auto px-6 md:px-12">
+                <div className="mx-auto px-6 xl:container md:px-12">
                     <div className="mb-16 md:w-2/3 lg:w-1/2">
                         <h2 className="mb-4 text-2xl font-bold">
                             Got questions?
                         </h2>
                     </div>
-                    <div className="grid gap-6 px-4 sm:px-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                        <div className="relative rounded-xl h-fit overflow-hidden">
+                    <div className="grid gap-6 px-4 sm:grid-cols-2 sm:px-0 md:grid-cols-3 lg:grid-cols-4">
+                        <div className="relative h-fit overflow-hidden rounded-xl">
                             <Image
-                                className="mx-auto w-full object-cover object-top ransition duration-500 group-hover:scale-105 group-hover:grayscale-0"
+                                className="ransition mx-auto w-full object-cover object-top duration-500 group-hover:scale-105 group-hover:grayscale-0"
                                 src="/img/photos/pastors.jpg"
                                 alt="Pastors Any and Emily Rattenbury"
                                 loading="lazy"
                                 width={558}
                                 height={815}
                             />
-                            <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-6 py-6 bg-brand-black transition duration-300 ease-in-out text-white">
+                            <div className="bg-brand-black absolute inset-x-0 bottom-0 mt-auto h-max px-6 py-6 text-white transition duration-300 ease-in-out">
                                 <div>
                                     <h4 className="text-xl font-semibold text-white">
                                         Andy &amp; Emily
@@ -39,22 +39,22 @@ export default function Page() {
                                     <span className="block text-sm">
                                         Pastors
                                     </span>
-                                    <span className="block text-sm mt-2">
+                                    <span className="mt-2 block text-sm">
                                         cheltenham@freedomchurch.cc
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div className="relative rounded-xl h-fit overflow-hidden">
+                        <div className="relative h-fit overflow-hidden rounded-xl">
                             <Image
-                                className="mx-auto h-fu"
+                                className="h-fu mx-auto"
                                 src="/img/photos/lorah.jpg"
                                 alt=""
                                 loading="lazy"
                                 width={558}
                                 height={815}
                             />
-                            <div className="px-6 py-6 bg-brand-black text-white">
+                            <div className="bg-brand-black px-6 py-6 text-white">
                                 <div>
                                     <h4 className="text-xl font-bold text-white">
                                         Lorah
@@ -62,22 +62,22 @@ export default function Page() {
                                     <span className="block text-sm">
                                         Kids Pastor
                                     </span>
-                                    <span className="block text-sm mt-2">
+                                    <span className="mt-2 block text-sm">
                                         ctm.kids@freedomchurch.cc
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div className="relative rounded-xl h-fit overflow-hidden">
+                        <div className="relative h-fit overflow-hidden rounded-xl">
                             <Image
-                                className="mx-auto h-fu"
+                                className="h-fu mx-auto"
                                 src="/img/photos/charlie-img.jpg"
                                 alt=""
                                 loading="lazy"
                                 width={558}
                                 height={815}
                             />
-                            <div className="px-6 py-6 bg-brand-black text-white">
+                            <div className="bg-brand-black px-6 py-6 text-white">
                                 <div>
                                     <h4 className="text-xl font-bold text-white">
                                         Charlie
@@ -85,22 +85,22 @@ export default function Page() {
                                     <span className="block text-sm">
                                         Volunteer Coordinator Pastor
                                     </span>
-                                    <span className="block text-sm mt-2">
-                                        ctm.volunteering@freedomchurch.cc
+                                    <span className="mt-2 block text-sm">
+                                        ctm.volunteers@freedomchurch.cc
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div className="relative rounded-xl h-fit overflow-hidden">
+                        <div className="relative h-fit overflow-hidden rounded-xl">
                             <Image
-                                className="mx-auto h-fu"
+                                className="h-fu mx-auto"
                                 src="/img/photos/john-and-abi-img.jpg"
                                 alt=""
                                 loading="lazy"
                                 width={558}
                                 height={815}
                             />
-                            <div className="px-6 py-6 bg-brand-black text-white">
+                            <div className="bg-brand-black px-6 py-6 text-white">
                                 <div>
                                     <h4 className="text-xl font-bold text-white">
                                         John &amp; Abi
@@ -108,7 +108,7 @@ export default function Page() {
                                     <span className="block text-sm">
                                         Zeal Pastors
                                     </span>
-                                    <span className="block text-sm mt-2">
+                                    <span className="mt-2 block text-sm">
                                         ctm.zeal@freedomchurch.cc
                                     </span>
                                 </div>
