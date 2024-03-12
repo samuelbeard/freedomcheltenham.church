b846bcd3caf16f96a71f21d61a97da5b92e77540
Samuel Beard <sam@charisma.ai>
2024-03-12 13:50:26 +0000

    fix again

--- components/WeMovedBanner.tsx
+++ components/WeMovedBanner.tsx
@@ -3,7 +3,7 @@ const WeMovedBanner = () => {
     const closeDate = new Date("2024-04-17")
 
     return closeDate > date ? (
-        <div className="z-10 bg-brand-yellow w-screen xh-32 text-brand-black text-center p-4 animate-bounce-short">
+        <div className="z-10 bg-brand-yellow w-full text-brand-black text-center p-4 animate-bounce-short">
             <h1 className="text-5xl font-bold ">
                 We have moved to a new location!
             </h1>
