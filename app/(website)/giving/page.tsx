4eff2f922aa2ce769484aa8d6819d7650e471ec5
Samuel Beard <sam@charisma.ai>
2025-02-04 16:51:58 +0000

    start work

--- app/(website)/giving/page.tsx
+++ app/(website)/giving/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { FaChevronRight } from "react-icons/fa"
 
 export const metadata = {
   title: "Giving",
@@ -18,6 +19,40 @@ export default function Page() {
         >
           Give Online Here
         </Link>
+      </section>
+      <section className="section pb-10 pt-2">
+        <div className="flex justify-around">
+          <div className="w-5/12 center">
+            <h3 className="h3">Bank account for tithe and donations</h3>
+          </div>
+          <div className="w-1/12 center">
+            <FaChevronRight className="h-6 w-6" />
+          </div>
+          <div className="w-6/12 center">
+            <div>
+              <p className="para">Make a payment direct to our bank account</p>
+              <p className="para">
+                Account name:{" "}
+                <span className="font-bold">Freedom Church Cheltenham</span>
+                <br />
+                Sort code:{" "}
+                <span className="font-bold">
+                  {process.env.NEXT_PUBLIC_BANK_SORT_CODE}
+                </span>
+                <br />
+                Account number:{" "}
+                <span className="font-bold">
+                  {process.env.NEXT_PUBLIC_BANK_ACCOUNT_NUMBER}
+                </span>
+              </p>
+              <p className="para">
+                You can also use these details to arrange a regular standing
+                order through internet or telephone banking, or at your local
+                branch.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
       <div className="section pb-10 pt-2">
         <div className="rounded bg-gray-100 p-10 lg:w-1/2">
