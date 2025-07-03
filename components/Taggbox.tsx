02dec60d31a38090014deaaa0009fa4ab75fa0e5
Samuel Beard <sam@lts.digital>
2025-07-03 16:53:56 +0100

    refactor: update Taggbox component structure and script import

--- components/Taggbox.tsx
+++ components/Taggbox.tsx
@@ -1,12 +1,16 @@
+"use client";
+
+import Script from "next/script";
+
 export default function Taggbox() {
   return (
     <>
       <div
-        className="taggbox w-full h-full overflow-auto p-4"
+        className="container mx-auto taggbox w-full h-full overflow-auto pt-10"
         data-widget-id="289926"
         {...({ website: "1" } as any)}></div>
 
-      <script src="https://widget.taggbox.com/embed.min.js" type="text/javascript"></script>
+      <Script src="https://widget.taggbox.com/embed.min.js" />
     </>
-  )
+  );
 }
