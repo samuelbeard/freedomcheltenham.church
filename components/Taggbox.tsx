017b3e6f419e0a0522a5acae421e75a022fe9aac
Dominic Stewart-Smith <dominicstewartsmith@gmail.com>
2025-07-03 09:24:32 +0100

    fix sizing

--- components/Taggbox.tsx
+++ components/Taggbox.tsx
@@ -1,7 +1,12 @@
 export default function Taggbox() {
   return (
-    <div className="w-full h-[850px]">
-      <iframe src="https://widget.taggbox.com/289926?website=1" style={{ "width": "100%", "height": "100%", "overflow": "auto", "border": "none" }}></iframe>
-    </div>
+    <>
+      <div
+        className="taggbox w-full h-full overflow-auto p-4"
+        data-widget-id="289926"
+        {...({ website: "1" } as any)}></div>
+
+      <script src="https://widget.taggbox.com/embed.min.js" type="text/javascript"></script>
+    </>
   )
 }
