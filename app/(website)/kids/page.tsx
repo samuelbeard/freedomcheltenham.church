f47d6d7e2fddf9f9c639c65b5686ec8048841d13
Samuel Beard <sam@lts.digital>
2025-10-01 12:28:05 +0100

    refactor kids page layout for improved structure and readability Resolves #75

--- app/(website)/kids/page.tsx
+++ app/(website)/kids/page.tsx
@@ -14,46 +14,32 @@ export default function Page() {
         </Link>
       </section>
       <div className="container mt-12 mb-16">
-        <section className="mx-auto max-w-screen-2xl px-4 md:px-8">
-          <div className="mb-8 flex flex-wrap justify-between md:mb-16">
-            <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
-              <Image
-                src="/img/kids/kids-logo-vertical.svg"
-                width={200}
-                height={100}
-                alt="Freedom Kids"
-              />
-              <p className="max-w-md leading-relaxed xl:text-xl">
-                <strong>Welcome to Freedom Kids!</strong>
-                <br /> We're all about helping your kids explore what it means
-                to follow Jesus in a way that makes sense to them.
-              </p>
-            </div>
-
-            <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-              <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
-                <Image
-                  loading="lazy"
-                  alt=""
-                  className="h-full w-full object-cover object-center"
-                  width={4021}
-                  height={3217}
-                  src="/img/kids/kids-1.jpg"
-                />
-              </div>
-
-              <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
-                <Image
-                  loading="lazy"
-                  alt=""
-                  className="h-full w-full object-cover object-center"
-                  width={6026}
-                  height={4024}
-                  src="/img/kids/kids-2.jpg"
-                />
-              </div>
-            </div>
+        <section className="flex flex-col md:flex-row justify-between items-center">
+          <div className="w-1/3">
+            <Image
+              src="/img/kids/kids-logo-vertical.svg"
+              width={400}
+              height={200}
+              alt="Freedom Kids"
+            />
           </div>
+          <div className="md:w-2/3">
+            <Image
+              loading="lazy"
+              alt=""
+              className="rounded w-full"
+              width={6026}
+              height={4024}
+              src="/img/kids/kids-2.jpg"
+            />
+          </div>
+        </section>
+        <section className="mt-10 mb-10 max-w-md mx-auto">
+          <h3 className="h3 text-center">Welcome to Freedom Kids!</h3>
+          <p className="para text-center">
+            We're all about helping your kids explore what it means to follow
+            Jesus in a way that makes sense to them.
+          </p>
         </section>
         <div className="mx-auto">
           <h3 className="h3">Relevant content for all age groups</h3>
