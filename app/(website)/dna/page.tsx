645388c8e62edf95b7afad90082ba59845c1ce06
Dominic Stewart-Smith <dominicstewartsmith@gmail.com>
2025-10-23 16:23:20 +0100

    bring title section in line with dnas on mobile

--- app/(website)/dna/page.tsx
+++ app/(website)/dna/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
           <h1 className="m-0 text-center text-6xl font-bold uppercase leading-none text-white md:-mt-10 md:text-[250px]">
             DNA
           </h1>
-          <p className="mx-auto mt-8 text-lg text-stone-200 lg:w-8/12">
+          <p className="mx-5 sm:mx-auto mt-8 text-lg text-stone-200 lg:w-8/12">
             The Freedom Church DNA are the core values of our community. Like a
             compass they point us towards our identity. We are committed to
             wholeheartedly practicing and developing these principles in our own
